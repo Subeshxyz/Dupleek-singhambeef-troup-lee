6e6cf93741f53ee7a4791c393e12bd14f4849ef5
Subeshxyz <subhamallahxyz@gmail.com>
2024-12-26 06:00:22 +0500

    Bista poor cubilla and flipper tobik sita
    Bista poor cubilla and flipper tobik sita with commentations belive able things non behaviuour parting

---
+++ Bista poor cubilla and flipper tobik sita.cpp
@@ -0,0 +1,20 @@
+#ifndef _DLL_H_
+#define _DLL_H_
+
+#if BUILDING_DLL
+#define DLLIMPORT __declspec(dllexport)
+#else
+#define DLLIMPORT __declspec(dllimport)
+#endif
+#git and gota will be able to connectivety of dll with coinfiguration ois nbbppe bukhe nidggfks flipd od ssp hyu
+# hey mistar gulan khan how are you i am there but i think you are not there after the slippling moood on you cant come back after few hour
+#everything is learning to join our products and projects is the most popular thing in the word of the crack wire soon
+class DLLIMPORT DllClass
+{
+	public:
+		DllClass();
+		virtual ~DllClass();
+		void HelloWorld();
+};
+
+#endif
